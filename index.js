89ddf09f7873ca5ca6b2993aaada91e0f838c68d
mubarek78 <shifamubarek@yahoo.com>
2022-03-26 20:24:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(names, str){
+    return( names.filter(function(name){
+         return name.toLowerCase() === str.toLowerCase()
+     }))
+ }
+ 
+ function fuzzyMatch(names, str){
+     return(
+         names.filter(function(name){
+             return name[0] === str[0]
+         })
+     )
+ }
+ 
+ function matchName(names, str){
+     return(
+         names.filter(function(name){
+             return name.name === str
+         })
+     )
+ }
